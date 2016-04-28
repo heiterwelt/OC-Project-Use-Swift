34bfa668f0954dbdb98655b4cc6fa4818b41fdf6
1361579531@qq.com <1361579531@qq.com>
2016-04-28 10:09:01 +0800

    init

---
+++ setup/setup/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  setup
+//
+//  Created by Linyou-IOS-1 on 16/4/28.
+//  Copyright © 2016年 MuYou INTERACTIVE TECHNOLOGY LIMITED. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
