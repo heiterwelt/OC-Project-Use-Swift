939baf7096c611599c4f9d198e695e31d9937d03
1361579531@qq.com <1361579531@qq.com>
2016-04-28 10:18:51 +0800

    init

---
+++ OCUseSwift/OCUseSwift/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  OCUseSwift
+//
+//  Created by Linyou-IOS-1 on 16/4/28.
+//  Copyright © 2016年 MuYou INTERACTIVE TECHNOLOGY LIMITED. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "OCUseSwift-swift.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    HNSwift *swift=[[HNSwift alloc]init];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
